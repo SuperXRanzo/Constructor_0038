d3dece52e96df3dee4d8b85e653bd57a7bf5f7db
SuperXRanzo <okanskyavenger@gmail.com>
2024-05-14 18:51:36 +0700

    end construcktor

--- construktor/Construktor.cpp
+++ construktor/Construktor.cpp
@@ -1,3 +1,35 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+private:
+    int Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    int Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+    int Mahasiswa(string iNama)
+    {
+        nama = iNama;
+    };
+    int Mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
+    void cetak() {
+        cout << "NIM  = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+    }
+};
